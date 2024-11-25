5286cce4d5b04c5821f8ac770c595d3675a847d0
Jules Dupuis <jules.dupuis@ynov.com>
2024-11-25 15:38:47 +0100

    first screen test

---
+++ app/(tutorial)/welcome.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,11 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        {
+          // isTutorialComplete ? null : (
+        <Stack.Screen name="(tutorial)" options={{ headerShown: false }} />
+          // )
+        }
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
