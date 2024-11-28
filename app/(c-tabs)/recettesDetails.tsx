87c5d7495f754fb2fd6252c711c7a64d5f4af156
Jules Dupuis <jules.dupuis@ynov.com>
2024-11-28 17:08:25 +0100

    cleaning

--- app/(c-tabs)/recettesDetails.tsx
+++ app/(c-tabs)/recettesDetails.tsx
@@ -1,8 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { RouteProp } from '@react-navigation/native';
 
-export default function recettesDetails(props: RecettesDetailsProps) {
+export default function recettesDetails() {
   return (
     <View>
       <Text>Détail de la recette</Text>
